37888b2f67e800ed14f3f6302721362cfa47fcd8
hgcastillo <hcastillo3@academic.rrc.ca>
2026-01-17 01:22:39 -0600

    full fill the Lab 1.2 requirements

---
+++ pixell-river-react/src/components/Header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => (
+  <header>
+    <a href="https://itsm-ace.ca/images/logo.svg" target="_blank" rel="noreferrer">
+      <img src="/logo.svg" alt="Pixell River Logo" className="logo" />
+    </a>
+    <div className="header-text">
+      <h1>Pixell River Employee Directory</h1>
+      <p>Welcome to the Pixell River Financial staff and organization directory.</p>
+    </div>
+  </header>
+);
